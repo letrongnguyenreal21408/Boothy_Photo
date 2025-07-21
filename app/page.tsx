da1ca7a78a493a5a3ba59fda2511bf3b0529ffa8
Lê Trọng Nguyên Real <153912865+letrongnguyenreal21408@users.noreply.github.com>
2025-07-21 16:29:24 +0700

    Delete app/page.tsx

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { WebcamCapture } from "./webcam-capture";
-
-export default function CameraPage() {
-  return (
-    <main className="place-self-center px-5">
-      <WebcamCapture />
-    </main>
-  );
-}
